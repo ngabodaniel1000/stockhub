38822f7919f3ec47850cd6e5917a460f609462bc
ngabodaniel1000 <ngabodaniel1000@gmail.com>
2025-01-18 16:32:08 +0200

    failed to connect backend to react

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,28 @@
 import React from 'react'
+import { BrowserRouter as Router,Routes, Route, Link } from 'react-router-dom'
+import Login from './components/Login'
+import Dashboard from './components/Dashboard'
+
 
 function App() {
   return (
     <div>
-       <h1 className="text-3xl text-green-400 font-bold underline">
-      Hello world!
-    </h1>
+      <Router>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Login</Link>
+            </li>
+            <li>
+              <Link to="/dashboard">Dashboard</Link>
+            </li>
+          </ul>
+          </nav>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </Router>
     </div>
   )
 }
--- server/app.js
+++ server/app.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 8889
 
 app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.get("/users",(req,res)=>{
+    res.json([{name:"john",age:30,location:"USA"},{name:"jane",age:25,location:"UK"}])
+})
+app.listen(port, () => console.log(`App is listening on port ${port}!`))
